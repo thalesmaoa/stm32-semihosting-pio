aaadd91d9a4217d7d370d578b529cd734d145c05
Thales Maia <thalesmaia@gmail.com>
2026-01-06 18:28:21 -0300

    Add function to work with internal clock

---
+++ src/src/clock_config.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+// --- TRATAMENTO DE CONFLITO ---
+// O Arduino define Error_Handler como macro. Removemos para usar a sua função.
+// #ifdef Error_Handler
+//   #undef Error_Handler
+// #endif
+
+// // Avisa ao compilador que esta função existe em outro lugar (no main.cpp)
+// void Error_Handler(void);
+
+// A função mágica que o sistema chama no boot
+extern "C" void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,16 +1,27 @@
 #include <Arduino.h>
 int count = 0;
 
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN PC13
+#endif
 
 extern "C" void initialise_monitor_handles(void);
 
 void setup()
 {
+  // SystemClock_Config();
   initialise_monitor_handles();
   printf("Hello world!\n");
+  printf("SystemCoreClock (Var): ");
+  printf("Clock: %lu Hz\n", SystemCoreClock);
+
+  // Método 2: Cálculo real baseado nos registradores (A verdade do Hardware)
+  printf("HAL Real Freq: ");
+  printf("Clock: %lu Hz\n", HAL_RCC_GetSysClockFreq());
 }
 
 void loop() {
   delay(500);
   printf("count = %d\n", count++);
 }
+
